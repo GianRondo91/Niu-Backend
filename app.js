77e5b8cd9548b0bde591c6c06d244d5ec78d0150
Giancarlo <giancarlorondo@outlook.com>
2021-05-09 14:23:43 +0200

    change port for heroku

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const router = require('./router');
 const db = require('./db');
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
